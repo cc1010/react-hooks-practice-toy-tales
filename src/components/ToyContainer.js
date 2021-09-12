927378adb71da8ceb2932ec1574c5aead10bf539
chaim <chaimyisroel@gmail.com>
2021-09-12 13:10:04 -0400

    commit tree a6f209d841fa2565132d3838406d4db3d0b82e4f

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
-  return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-  );
+function ToyContainer({ list, addLikes, deleteToy }) {
+	return (
+		<div id="toy-collection">
+			{/* Render the collection of ToyCards */}
+			{list.map((item) => (
+				<ToyCard
+					key={item.id}
+					item={item}
+					addLikes={addLikes}
+					deleteToy={deleteToy}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default ToyContainer;
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
-  return (
-    <div className="container">
-      <form className="add-toy-form">
-        <h3>Create a toy!</h3>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter a toy's name..."
-          className="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="image"
-          placeholder="Enter a toy's image URL..."
-          className="input-text"
-        />
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create New Toy"
-          className="submit"
-        />
-      </form>
-    </div>
-  );
+function ToyForm({ addToy }) {
+	const [formData, setFormData] = useState({ name: "", image: "" });
+	function handleForm(e) {
+		const key = e.target.name;
+		const value = e.target.value;
+		setFormData({ ...formData, [key]: value });
+	}
+	console.log(formData);
+	function handleSubmit(e) {
+		e.preventDefault();
+		addToy(formData);
+	}
+	return (
+		<div className="container">
+			<form className="add-toy-form">
+				<h3>Create a toy!</h3>
+				<input
+					type="text"
+					name="name"
+					placeholder="Enter a toy's name..."
+					className="input-text"
+					value={formData.name}
+					onChange={handleForm}
+				/>
+				<br />
+				<input
+					type="text"
+					name="image"
+					placeholder="Enter a toy's image URL..."
+					className="input-text"
+					value={formData.image}
+					onChange={handleForm}
+				/>
+				<br />
+				<input
+					type="submit"
+					name="submit"
+					value="Create New Toy"
+					className="submit"
+					onClick={handleSubmit}
+				/>
+			</form>
+		</div>
+	);
 }
 
 export default ToyForm;
